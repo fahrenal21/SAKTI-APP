9a98e266cacc16491bcb1a5e0a9c7ddbddc30b0a
Muhammad Fahrenal Azzam <fahrenal.a@gmail.com>
2025-05-16 14:18:06 +0700

    ganti teks menjadi SAKTI-APP

--- resources/views/password/verify.blade.php
+++ resources/views/password/verify.blade.php
@@ -374,8 +374,8 @@
                                 <table role="presentation" border="0" cellpadding="0" cellspacing="0">
                                     <tr>
                                         <td>
-                                            <h3> Reset Password Akun SIPEGAWAI </h3>
-                                            <h4>Link Untuk Reset Password Akun SIPEGAWAI Kamu Ada Di Bawah Sini.</h4>
+                                            <h3> Reset Password Akun SAKTI-APP </h3>
+                                            <h4>Link Untuk Reset Password Akun SAKTI-APP Kamu Ada Di Bawah Sini.</h4>
                                             <table role="presentation" border="0" cellpadding="0" cellspacing="0"
                                                 class="btn btn-primary">
                                                 <tbody>
@@ -415,7 +415,7 @@
                         <table role="presentation" border="0" cellpadding="0" cellspacing="0">
                             <tr>
                                 <td class="content-block">
-                                    <span class="apple-link">SIPEGAWAI</span>
+                                    <span class="apple-link">SAKTI-APP</span>
                                     <br> Email Notification <a href="#"></a>.
                                 </td>
                             </tr>
