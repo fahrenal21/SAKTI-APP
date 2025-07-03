ccda4017d221ed848464c16f3621673a12e2e100
Muhammad Fahrenal Azzam <fahrenal.a@gmail.com>
2025-07-03 15:42:37 +0700

    ubah login template

--- resources/views/login/base.blade.php
+++ resources/views/login/base.blade.php
@@ -5,66 +5,27 @@
     <title>@yield('title')</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!--===============================================================================================-->
-    <link rel="shortcut icon" href="{{ URL::to('/img') }}/icon.png" type="image/x-icon">
-    <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css"
-        href="{{ URL::to('/login_template') }}/vendor/bootstrap/css/bootstrap.min.css">
-    <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css"
-        href="{{ URL::to('/login_template') }}/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
-    <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css"
-        href="{{ URL::to('/login_template') }}/fonts/iconic/css/material-design-iconic-font.min.css">
-    <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="{{ URL::to('/login_template') }}/vendor/animate/animate.css">
-    <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css"
-        href="{{ URL::to('/login_template') }}/vendor/css-hamburgers/hamburgers.min.css">
-    <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css"
-        href="{{ URL::to('/login_template') }}/vendor/animsition/css/animsition.min.css">
-    <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="{{ URL::to('/login_template') }}/vendor/select2/select2.min.css">
-    <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css"
-        href="{{ URL::to('/login_template') }}/vendor/daterangepicker/daterangepicker.css">
-    <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="{{ URL::to('/login_template') }}/css/util.css">
-    <link rel="stylesheet" type="text/css" href="{{ URL::to('/login_template') }}/css/main.css">
-    <!--===============================================================================================-->
-</head>
+    
+    {{-- Icon --}}
+    <link rel="shortcut icon" href="{{ URL::to('/img') }}/logo.png" type="image/x-icon">
+    
+    {{-- CSS dari Template Baru --}}
+    <link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    
+    {{-- CSS Kustom Anda --}}
+    <link rel="stylesheet" type="text/css" href="{{ asset('login_new/css/style.css') }}">
+    </head>
 
 <body>
-
+    
     @include('sweetalert::alert')
-
-    <div class="container-login100"
-        style="background-image: url('{{ URL::to('/login_template') }}/images/bg-02.gif');">
-        @yield('content')
-    </div>
+    @yield('content')
 
 
-
-    <div id="dropDownSelect1"></div>
-
-    <!--===============================================================================================-->
-    <script src="{{ URL::to('/login_template') }}/vendor/jquery/jquery-3.2.1.min.js"></script>
-    <!--===============================================================================================-->
-    <script src="{{ URL::to('/login_template') }}/vendor/animsition/js/animsition.min.js"></script>
-    <!--===============================================================================================-->
-    <script src="{{ URL::to('/login_template') }}/vendor/bootstrap/js/popper.js"></script>
-    <script src="{{ URL::to('/login_template') }}/vendor/bootstrap/js/bootstrap.min.js"></script>
-    <!--===============================================================================================-->
-    <script src="{{ URL::to('/login_template') }}/vendor/select2/select2.min.js"></script>
-    <!--===============================================================================================-->
-    <script src="{{ URL::to('/login_template') }}/vendor/daterangepicker/moment.min.js"></script>
-    <script src="{{ URL::to('/login_template') }}/vendor/daterangepicker/daterangepicker.js"></script>
-    <!--===============================================================================================-->
-    <script src="{{ URL::to('/login_template') }}/vendor/countdowntime/countdowntime.js"></script>
-    <!--===============================================================================================-->
-    <script src="{{ URL::to('/login_template') }}/js/main.js"></script>
-
-</body>
+    {{-- JS dari Template Baru --}}
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js"></script>
+    </body>
 
 </html>
--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -3,35 +3,66 @@
 @section('title', 'SAKTI | Login')
 
 @section('content')
-    <div class="wrap-login100 p-l-55 p-r-55 p-t-80 p-b-30">
-        <form action="{{ route('login') }}" method="POST" class="login100-form validate-form">
-            @csrf
-
-            <span class="login100-form-title p-b-37">
-                SAKTI
-            </span>
-
-            <div class="wrap-input100 validate-input m-b-20" data-validate="Enter username or email">
-                <input class="input100" type="text" name="email" placeholder="Email">
-                <span class="focus-input100"></span>
+{{-- 1. TAMBAHKAN 3 CLASS INI untuk membuat container utama menjadi flexbox --}}
+<div class="container-fluid px-1 px-md-5 px-lg-1 px-xl-5 py-5 mx-auto d-flex flex-column min-vh-100">
+    <div class="card card0 border-0">
+        <div class="row d-flex">
+            <div class="col-lg-6">
+                <div class="card1 pb-5">
+                    <div class="row">
+                        {{-- Ganti 'img/logo.png' dengan path logo Anda di folder public --}}
+                        <img src="{{ asset('img/logo.png') }}" class="logo">
+                    </div>
+                    <div class="row px-3 justify-content-center mt-4 mb-5 border-line">
+                        {{-- Ganti 'img/vector.png' dengan path gambar Anda di folder public --}}
+                        <img src="{{ asset('img/vector.png') }}" class="image">
+                    </div>
+                </div>
             </div>
+            <div class="col-lg-6">
+                <div class="card2 card border-0 px-4 py-5">
+                    
+                    {{-- Form Login Laravel --}}
+                    <form action="{{ route('login') }}" method="POST">
+                        @csrf
 
-            <div class="wrap-input100 validate-input m-b-25" data-validate="Enter password">
-                <input class="input100" type="password" name="password" placeholder="Password">
-                <span class="focus-input100"></span>
-            </div>
+                        {{-- Input Email --}}
+                        <div class="row px-3">
+                            <label class="mb-1"><h6 class="mb-0 text-sm">Email Address</h6></label>
+                            <input class="mb-4" type="email" name="email" placeholder="Enter a valid email address" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                        </div>
 
-            <div class="container-login100-form-btn">
-                <button class="login100-form-btn">
-                    Sign In
-                </button>
+                        {{-- Input Password --}}
+                        <div class="row px-3">
+                            <label class="mb-1"><h6 class="mb-0 text-sm">Password</h6></label>
+                            <input type="password" name="password" placeholder="Enter password" required autocomplete="current-password">
+                        </div>
+                        
+                        <div class="row px-3 mb-4">
+                            <div class="custom-control custom-checkbox custom-control-inline">
+                                <input id="chk1" type="checkbox" name="remember" class="custom-control-input"> 
+                                <label for="chk1" class="custom-control-label text-sm">Remember me</label>
+                            </div>
+                            {{-- Link Lupa Password --}}
+                            <a href="{{ route('forget-password.getEmail') }}" class="ml-auto mb-0 text-sm">Forgot Password?</a>
+                        </div>
+
+                        {{-- Tombol Sign In --}}
+                        <div class="row mb-3 px-3">
+                            <button type="submit" class="btn btn-blue text-center">Login</button>
+                        </div>
+                    </form>
+
+                </div>
             </div>
-            <div class="mt-3"></div>
-            <div class="text-center">
-                <a href="{{ route('forget-password.getEmail') }}" class="txt2 hov1">
-                    Lupa Password?
-                </a>
-            </div>
-        </form>
+        </div>
     </div>
+    
+    {{-- 2. TAMBAHKAN CLASS 'mt-auto' PADA DIV FOOTER INI --}}
+    <div class="mt-auto">
+        <div class="row px-3 justify-content-center">
+            <small class="ml-4 ml-sm-5 mb-2">Copyright &copy; SAKTI {{ date('Y') }}. All rights reserved.</small>
+        </div>
+    </div>
+</div>
 @endsection
